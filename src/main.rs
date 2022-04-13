6220b475a553bcc67b1ecf6a111bef58ec46f540
StackDoubleFlow <ojaslandge@gmail.com>
2022-04-13 16:54:22 -0500

    Support for tracing adrp

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 #![feature(bool_to_option)]
 
-use anyhow::{anyhow, bail, ensure, Result, Context};
-use bad64::{disasm, Imm, Op, Operand};
+use anyhow::{anyhow, bail, ensure, Context, Result};
+use bad64::{disasm, Imm, Op, Operand, Reg};
+use clap::Parser;
 use object::elf::{STT_FUNC, STT_OBJECT};
-use object::{Object, ObjectSymbol, SymbolFlags};
-use petgraph::EdgeDirection;
+use object::{Object, ObjectSection, ObjectSymbol, SymbolFlags};
 use petgraph::dot::Dot;
-use petgraph::graph::{DiGraph, NodeIndex, EdgeReference};
+use petgraph::graph::{DiGraph, EdgeReference, NodeIndex};
 use petgraph::visit::EdgeRef;
+use petgraph::EdgeDirection;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::{fmt, fs};
-use clap::Parser;
 
 #[derive(Debug, PartialEq, Eq)]
 enum SymbolType {
@@ -36,14 +36,20 @@ impl fmt::Debug for Symbol<'_> {
 enum RefType {
     B,
     Bl,
+    PcRel { adrp_num: usize },
 }
 
 impl fmt::Display for RefType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", match self {
-            RefType::B => "b",
-            RefType::Bl => "bl",
-        })
+        write!(
+            f,
+            "{}",
+            match self {
+                RefType::B => "b",
+                RefType::Bl => "bl",
+                RefType::PcRel { .. } => "pcRelData",
+            }
+        )
     }
 }
 
@@ -55,7 +61,13 @@ struct Ref {
 
 impl fmt::Debug for Ref {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}({})", self.ty, self.num)
+        write!(f, "{}", self.ty)?;
+        match self.ty {
+            RefType::B | RefType::Bl => write!(f, "(num: {})", self.num),
+            RefType::PcRel { adrp_num } => {
+                write!(f, "(num_adrp: {}, ldr_num_offset: {})", adrp_num, self.num)
+            }
+        }
     }
 }
 
@@ -67,7 +79,11 @@ struct GraphInfo<'obj> {
     symbol_map: HashMap<u64, NodeIndex>,
 }
 
-fn gen_graph<'obj>(bin_data: &[u8], obj_file: &'obj object::File) -> Result<GraphInfo<'obj>> {
+fn gen_graph<'obj>(
+    bin_data: &[u8],
+    obj_file: &'obj object::File,
+    ignore_sections: HashSet<String>,
+) -> Result<GraphInfo<'obj>> {
     let mut graph = Graph::new();
     let mut symbol_map = HashMap::new();
     let mut name_map = HashMap::new();
@@ -84,6 +100,11 @@ fn gen_graph<'obj>(bin_data: &[u8], obj_file: &'obj object::File) -> Result<Grap
             },
             _ => bail!("Symbol flags not elf format"),
         };
+        if let Some(section_idx) = symbol.section_index() {
+            if ignore_sections.contains(obj_file.section_by_index(section_idx)?.name()?) {
+                continue;
+            }
+        }
         let demangled = if let Ok(demangle) = cpp_demangle::Symbol::new(symbol.name_bytes()?) {
             demangle.to_string()
         } else {
@@ -111,17 +132,61 @@ fn gen_graph<'obj>(bin_data: &[u8], obj_file: &'obj object::File) -> Result<Grap
         let decoded = disasm(code, n.addr);
         let mut num_b = 0;
         let mut num_bl = 0;
+        let mut num_adrp = 0;
+        let mut num_ldr: HashMap<Reg, usize> = HashMap::new();
+        let mut adrp_map = HashMap::new();
         for ins in decoded {
             let ins = ins.map_err(|err| anyhow!("{}", err))?;
 
-            let (op, num) = match ins.op() {
-                Op::B => (RefType::B, &mut num_b),
-                Op::BL => (RefType::Bl, &mut num_bl),
+            let (op, num, target) = match ins.op() {
+                op @ (Op::B | Op::BL) => {
+                    let (op, num) = match op {
+                        Op::B => (RefType::B, &mut num_b),
+                        Op::BL => (RefType::Bl, &mut num_bl),
+                        _ => unreachable!(),
+                    };
+                    let target = match ins.operands()[0] {
+                        Operand::Label(Imm::Unsigned(addr)) => addr,
+                        _ => bail!("branch target not label"),
+                    };
+                    let this_num = *num;
+                    *num += 1;
+                    (op, this_num, target)
+                }
+                Op::ADRP => {
+                    let (reg, imm) = match ins.operands() {
+                        &[Operand::Reg { reg, .. }, Operand::Label(Imm::Unsigned(imm))] => {
+                            (reg, imm)
+                        }
+                        _ => bail!("adrp operand not reg"),
+                    };
+                    adrp_map.insert(reg, (num_adrp, imm, *num_ldr.entry(reg).or_default()));
+                    num_adrp += 1;
+                    continue;
+                }
+                Op::LDR => {
+                    let (reg, imm) = match ins.operands()[1] {
+                        Operand::MemOffset {
+                            reg,
+                            offset: Imm::Signed(imm),
+                            ..
+                        } => (reg, imm),
+                        _ => continue,
+                    };
+                    let adrp = match adrp_map.get(&reg) {
+                        Some(adrp) => *adrp,
+                        None => continue,
+                    };
+                    adrp_map.remove(&reg);
+                    let ty = RefType::PcRel { adrp_num: adrp.0 };
+                    let target = (adrp.1 as i64 + imm) as u64;
+                    let num_ldr = num_ldr.get_mut(&reg).unwrap();
+                    // TODO: will this always be 0 anyways?
+                    let this_num = *num_ldr - adrp.2;
+                    *num_ldr += 1;
+                    (ty, this_num, target)
+                }
                 _ => continue,
-            };
-            let target = match ins.operands()[0] {
-                Operand::Label(Imm::Unsigned(addr)) => addr,
-                _ => bail!("branch target not label"),
             };
             if let Some(&target) = symbol_map.get(&target) {
                 graph.add_edge(
@@ -129,22 +194,34 @@ fn gen_graph<'obj>(bin_data: &[u8], obj_file: &'obj object::File) -> Result<Grap
                     target,
                     Ref {
                         ty: op,
-                        num: *num,
+                        num,
                         offset: ins.address() - addr,
                     },
                 );
             }
-            *num += 1;
         }
     }
 
-    Ok(GraphInfo { graph, name_map, symbol_map })
+    Ok(GraphInfo {
+        graph,
+        name_map,
+        symbol_map,
+    })
 }
 
-fn search<'obj>(obj_file: &object::File, graph_info: &'obj GraphInfo, name: &str) -> Option<Vec<EdgeReference<'obj, Ref>>> {
+fn search<'obj>(
+    obj_file: &object::File,
+    graph_info: &'obj GraphInfo,
+    name: &str,
+) -> Option<Vec<EdgeReference<'obj, Ref>>> {
     let graph = &graph_info.graph;
-    let roots: HashSet<_> = obj_file.symbols().filter_map(|s| s.is_global().then_some(s.address())).collect();
+    let roots: HashSet<_> = obj_file
+        .symbols()
+        .filter_map(|s| s.is_global().then_some(s.address()))
+        .collect();
 
+    // Instead of keeping track of the paths like this, I should be keeping track of node parents,
+    // but whatever
     let mut queue: VecDeque<(NodeIndex, Vec<EdgeReference<_>>)> = VecDeque::new();
     queue.push_back((graph_info.name_map[name], Vec::new()));
     let mut visited: HashSet<NodeIndex> = HashSet::new();
@@ -162,7 +239,6 @@ fn search<'obj>(obj_file: &object::File, graph_info: &'obj GraphInfo, name: &str
                 path.push(e);
                 queue.push_back((source, path));
             }
-            
         }
     }
 
@@ -176,6 +252,9 @@ struct Args {
     /// Name of the symbol to trace
     #[clap(short, long)]
     name: String,
+    /// Comma seperated list of sections to ignore (ex: "il2cpp")
+    #[clap(short, long, default_value_t)]
+    ignore_sections: String,
 }
 
 fn main() -> Result<()> {
@@ -184,7 +263,12 @@ fn main() -> Result<()> {
     ensure!(obj_file.has_debug_symbols(), "no debug symbols were found");
 
     let args = Args::parse();
-    let graph_info = gen_graph(&bin_data, &obj_file)?;
+    let ignored_sections = args
+        .ignore_sections
+        .split(',')
+        .map(|s| s.to_string())
+        .collect();
+    let graph_info = gen_graph(&bin_data, &obj_file, ignored_sections)?;
 
     let dot = Dot::new(&graph_info.graph);
     fs::write("./data/graph.dot", format!("{:?}", dot))?;
@@ -200,6 +284,6 @@ fn finish(graph: &Graph, path: Vec<EdgeReference<Ref>>) {
     for (i, e) in path.iter().rev().enumerate() {
         let target = &graph[e.target()];
         let r = &graph[e.id()];
-        println!("{}: {}({}): {}", i, r.ty, r.num, target.demangled);
+        println!("{}: {:?}: {}", i, r, target.demangled);
     }
 }
